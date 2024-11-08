40a59dc48d095621e9800a079abb6829626afc07
Esteban Camargo <28689857+Estebmaister@users.noreply.github.com>
2024-11-08 13:01:04 -0500

    Alert file updated

--- src/helpers/alert.py
+++ src/helpers/alert.py
@@ -3,31 +3,33 @@
 import subprocess, sys
 from db import Publishers
 from typing import List
+from helpers.logger import logger
 
-default_content = "Update found"
-msg = dict( title = "Scrape alert", content = default_content, alert = 0 )
-alert_icon = "/usr/share/icons/Adwaita/scalable/"
-alert_icon += "status/software-update-urgent-symbolic.svg"
+log = logger(__name__)
+default_content = 'Update found'
+msg = dict( title = 'Scrape alert', content = default_content, alert = 0 )
+alert_icon = '/usr/share/icons/Adwaita/scalable/'
+alert_icon += 'status/software-update-urgent-symbolic.svg'
 
 def reminder(_send: bool = False):
-    if msg["alert"] == 0 and not _send:
+    if msg['alert'] == 0 and not _send:
         return
     try:
-        subprocess.Popen([ 'notify-send', "-i", alert_icon, "-u", "critical",
-            msg.get("title") + f" - ({ msg.get('alert') })", 
-            msg.get("content")])
+        subprocess.Popen([ 'notify-send', '-i', alert_icon, '-u', 'critical',
+            msg.get('title') + f' - ({ msg.get('alert') })', 
+            msg.get('content')])
     except FileNotFoundError:
-        print('MSG:', 'Notifier not found - ', msg.get("content"))
-    msg["alert"] = 0
-    msg["content"] = default_content
+        log.warning('Notifier not found - %s', msg.get('content'))
+    msg['alert'] = 0
+    msg['content'] = default_content
 
 def add_alert_to_msg(title: str, chap: str, publisher: List[Publishers]):
-    publishers_to_look = f"- {[Publishers(pub).name for pub in publisher]}"
-    update_msg = f"\t\n{title}, ch <b>{chap}</b> {publishers_to_look}"
+    publishers_to_look = f'- {[Publishers(pub).name for pub in publisher]}'
+    update_msg = f'\t\n{title}, ch <b>{chap}</b> {publishers_to_look}'
     print(title,chap,publishers_to_look)
-    msg["alert"] += 1
-    msg["content"] += update_msg
-    if msg["alert"] == 4:
+    msg['alert'] += 1
+    msg['content'] += update_msg
+    if msg['alert'] == 4:
         reminder()
 
 if 'alert' in sys.argv:
