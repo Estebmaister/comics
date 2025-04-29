29580b07d29dff2dcbd728b94e3c0c47669159ac
Esteban Camargo <28689857+Estebmaister@users.noreply.github.com>
2025-04-29 10:59:10 -0500

    Updating ca cert and keys

--- tls/encrypt.sh
+++ tls/encrypt.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 if [ "$1" = "d" ]; then
-    openssl aes-256-cbc -d -md sha512 -pbkdf2 -iter 1000000 -in comics.key.enc -out comics.key -pass pass:"$CA_KEY_PASSWORD"
+    openssl aes-256-cbc -md sha512 -pbkdf2 -iter 1000000 \
+    -pass pass:"$CA_KEY_PASSWORD" -salt -in comics.key.enc -out comics.key -d 
 else
-    openssl aes-256-cbc -md sha512 -pbkdf2 -iter 1000000 -salt -in comics.key -out comics.key.enc -pass pass:"$CA_KEY_PASSWORD"
+    openssl aes-256-cbc -md sha512 -pbkdf2 -iter 1000000 \
+    -pass pass:"$CA_KEY_PASSWORD" -salt -in comics.key -out comics.key.enc
 fi
